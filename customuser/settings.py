30a7a479301fb0e4b7c2d905c867612b9a9cbfe2
Koren Nyles <korenenyles@gmail.com>
2020-05-19 14:44:06 -0400

    finishing touches with css before submission

--- customuser/settings.py
+++ customuser/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'myuser.CustomUser'
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
--- myuser/urls.py
+++ myuser/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path('', views.homeView, name='home'),
-    path('', views.loginview, name='login'),
     path('signup/', views.signUpView, name='signup'),
     path('login/', views.loginview, name='login'),
     path('logout/', views.logout_view, name='logout')
